cd2c4b1cde4b3ba18e57e970782ded4ff42a53fa
hashes <79380426+eselokko@users.noreply.github.com>
2023-01-29 02:42:38 +0000

    Update loginhash.py

--- loginhash.py
+++ loginhash.py
@@ -7,8 +7,8 @@ os.system("clear")
 print("Bienvenido al sistema de verificacion")
 
 ctr=str(input("Coloque su contraseña: "))
-hashes=hashlib.sha256(bytes(ctr, encoding='utf-8')).hexdigest()
-if hashes=="hashdelacontraseña":
+hashes=hashlib.sha256(bytes(ctr, encoding='utf-8')).hexdigest() # la libreria soporta todo tipo de encriptación SHA, blake y MD5. si busca cambiarlo, reemplace 'sha256' por la encriptación disponible que desea.
+if hashes=="0573df7b697c8325c2716d00a57962c9c2077595940bf38248dfdd86efb2e001": # aca va tu hash (hash puesto: acavatuhash)
     print("Bienvenido usuario")
 else:
     print("Contraseña incorrecta, intentelo en otra oportunidad.")
